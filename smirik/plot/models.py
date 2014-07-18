156478c289174f38f326d085c9b8c7d5ba2d9ea7
Dmitry Kosolapov work self@dicos.ru <self@dicos.ru>
2014-07-18 15:38:16 +0700

    add model DataCache

--- smirik/plot/models.py
+++ smirik/plot/models.py
@@ -1,3 +1,17 @@
+#coding: utf8
 from django.db import models
 
-# Create your models here.
+from auth.models import NegotiablePaper
+
+
+class DataCache(models.Model):
+    """ cache data from yahoo finance service """
+    paper = models.ForeignKey(NegotiablePaper, verbose_name=u'name of paper')
+    date = models.DateField(verbose_name=u'date of ticket')
+    price = models.DecimalField(max_digits=8,
+                                decimal_places=6,
+                                verbose_name=u'price of negotiable paper')
+    
+    class Meta:
+        verbose_name = u'cache data by day portfolio'
+        unique_together = ('paper', 'date',)
