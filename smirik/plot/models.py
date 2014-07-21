c3e2c4d80025286229bd601098de0d9aa2b67c72
Dmitry Kosolapov work self@dicos.ru <self@dicos.ru>
2014-07-21 16:05:03 +0700

    need create plot!

--- smirik/plot/models.py
+++ smirik/plot/models.py
@@ -1,5 +1,6 @@
 #coding: utf8
 from django.db import models
+from django_pandas.managers import DataFrameManager
 
 from auth.models import NegotiablePaper
 
@@ -7,10 +8,11 @@ from auth.models import NegotiablePaper
 class DataCache(models.Model):
     """ cache data from yahoo finance service """
     paper = models.ForeignKey(NegotiablePaper, verbose_name=u'name of paper')
-    date = models.DateField(verbose_name=u'date of ticket')
+    date = models.DateField(verbose_name=u'date of ticket', db_index=True)
     price = models.DecimalField(max_digits=8,
                                 decimal_places=2,
                                 verbose_name=u'price of negotiable paper')
+    objects = DataFrameManager()
 
     def __unicode__(self):
         data = (self.paper, self.date.strftime("%Y-%m-%d"), self.price)
--- smirik/plot/views.py
+++ smirik/plot/views.py
@@ -1,15 +1,39 @@
 from io import BytesIO
 import base64
+from decimal import Decimal
 
 import matplotlib.pyplot as plt
 from django.shortcuts import render
-from pandas import Series
+from pandas import Series, DataFrame
+from numpy import array
 
 from plot.models import DataCache
 
 
 def main(request):
-    qs = DataCache.objects.filter(paper__client=request.user)
+    data = DataCache.objects.values('paper__name', 'date', 'price') \
+                            .order_by('paper' ,'date')
+    ticks = {}
+# TODO: need work it!
+    first_prices = {}
+    prices = array()
+    dates = array()
+    curr_paper = ''
+    for item in data:
+        if curr_paper != item['paper__name']:
+            first_prices
+            if len(prices) > 0:
+                ticks[curr_paper] = Series(prices, index=dates, name=curr_paper)
+            curr_paper = item['paper__name']
+            prices = array()
+            dates = array()
+        prices.append(item['price'])
+        dates.append(item['date'])
+    if len(array) > 0:
+        ticks[curr_paper] = Series(prices, index=dates, name=curr_paper)
+        del prices, dates, curr_paper
+
+
     Series.plot()
     base64.b64encode(image)
     st.seek(0)
